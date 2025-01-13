d738e18dce5c6ff4268d2be61ea9735fd9523c1c
rauljlle <rauljlle@gmail.com>
2025-01-12 22:00:11 -0300

    changes routes to parent folder

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from './modules/login/auth/routes';
+import authRoutes from './modules/login/routes';
 
 dotenv.config();
 
--- src/modules/login/routes.ts
+++ src/modules/login/routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { login, register } from './authController';
+import { login, register } from './auth/authController';
 
 const router = Router();
 
