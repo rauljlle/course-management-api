5c04e62885c990955dbf602b048877b0ebc5078b
rauljlle <rauljlle@gmail.com>
2025-01-13 02:18:21 -0300

    Adds course repository

---
+++ src/modules/courses/CourseRepository.ts
@@ -0,0 +1,27 @@
+import CourseModel from "./CourseModel";
+import ICourse from "./interfaces/ICourse";
+import { ICourseRepository } from "./interfaces/ICourseRepository";
+
+
+export class CourseRepository implements ICourseRepository {
+  async create(course: Partial<ICourse>): Promise<ICourse> {
+    return CourseModel.create(course);
+  }
+
+  async findAll(filters: Partial<ICourse> = {}): Promise<ICourse[]> {
+    return await CourseModel.find({filters});
+  }
+
+  async findById(id: string): Promise<ICourse | null> {
+    return CourseModel.findById(id);
+  }
+
+  async update(id: string, updates: Partial<ICourse>): Promise<ICourse | null> {
+    return CourseModel.findByIdAndUpdate(id, updates, { new: true });
+  }
+
+  async delete(id: string): Promise<boolean> {
+    const result = await CourseModel.findByIdAndDelete(id);
+    return result !== null;
+  }
+}
