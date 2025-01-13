5f227bb60facc5c914e5f55bc0df6fc792a57eb1
rauljlle <rauljlle@gmail.com>
2025-01-13 06:00:30 -0300

    remove dist

--- dist/modules/login/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const AuthController_1 = require("./auth/AuthController");
-const router = (0, express_1.Router)();
-router.post("/", AuthController_1.AuthController.login);
-router.post("/register", AuthController_1.AuthController.register);
-exports.default = router;
