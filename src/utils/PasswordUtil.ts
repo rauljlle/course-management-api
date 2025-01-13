47009d3b518713b1b55148509a69ab22ad037a47
rauljlle <rauljlle@gmail.com>
2025-01-12 23:35:27 -0300

    Creates PasswordUtil

---
+++ src/utils/PasswordUtil.ts
@@ -0,0 +1,15 @@
+import bcrypt from "bcrypt";
+
+export class PasswordUtils {
+  static async hashPassword(password: string): Promise<string> {
+    const saltRounds = 10;
+    return bcrypt.hash(password, saltRounds);
+  }
+
+  static async comparePasswords(
+    plainPassword: string,
+    hashedPassword: string
+  ): Promise<boolean> {
+    return bcrypt.compare(plainPassword, hashedPassword);
+  }
+}
