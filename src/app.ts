5f77be05a7536813f6969b32ed78bcbd38ed8277
rauljlle <rauljlle@gmail.com>
2025-01-13 04:31:30 -0300

    Adds default user on app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import authRoutes from "./modules/login/routes";
 import coursesRoutes from "./modules/courses/routes";
 import { connectInMemoryDB } from "./db/DBConnector";
+import UserModel from "./modules/login/user/UserModel";
 
 dotenv.config();
 
@@ -17,6 +18,14 @@ app.use("/courses", coursesRoutes);
 
 try{
     connectInMemoryDB();
+
+    UserModel.create({
+        email: "admin@admin.com",
+        name: "admin",
+        username: "admin",
+        password: "admin"
+    });
+
     app.listen(PORT, () => {
         console.log(`Server running on http://localhost:${PORT}`);
     });
